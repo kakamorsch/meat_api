7bf56b1cde60d8895e0ccd826225914ceb59b9c2
Kaio Vitor Morsch <kakamorsch@gmail.com>
2020-11-26 10:13:12 -0300

    applying DRY to mitigate code repetition

--- dist/users/users.routers.js
+++ dist/users/users.routers.js
@@ -4,30 +4,22 @@ exports.usersRouter = void 0;
 const router_1 = require("../common/router");
 const users_model_1 = require("./users.model");
 class UsersRouter extends router_1.Router {
+    constructor() {
+        super();
+        this.on("beforeRender", (document) => {
+            document.password = undefined;
+        });
+    }
     applyRoutes(application) {
         application.get("/users", (req, res, next) => {
-            users_model_1.User.find().then((users) => {
-                res.json(users);
-                return next();
-            });
+            users_model_1.User.find().then(this.render(res, next));
         });
         application.get("/users/:id", (req, res, next) => {
-            users_model_1.User.findById(req.params.id).then((user) => {
-                if (user) {
-                    res.json(user);
-                    return next();
-                }
-                res.send(404);
-                return next();
-            });
+            users_model_1.User.findById(req.params.id).then(this.render(res, next));
         });
         application.post("/users", (req, res, next) => {
             let user = new users_model_1.User(req.body);
-            user.save().then((user) => {
-                user.password = undefined;
-                res.json(user);
-                return next();
-            });
+            user.save().then(this.render(res, next));
         });
         application.put("/users/:id", (req, res, next) => {
             const options = { overwrite: true };
@@ -41,22 +33,11 @@ class UsersRouter extends router_1.Router {
                     res.send(404);
                 }
             })
-                .then((user) => {
-                res.json(user);
-                return next();
-            });
+                .then(this.render(res, next));
         });
         application.patch("/users/:id", (req, res, next) => {
             const options = { new: true };
-            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).then((user) => {
-                if (user) {
-                    res.json(user);
-                    return next();
-                }
-                else {
-                    res.send(404);
-                }
-            });
+            users_model_1.User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(res, next));
         });
         application.del("/users/:id", (req, res, next) => {
             users_model_1.User.deleteOne({ _id: req.params.id }).then((cmdResult) => {
--- users/users.routers.ts
+++ users/users.routers.ts
@@ -3,30 +3,23 @@ import * as restify from "restify";
 import { User } from "./users.model";
 
 class UsersRouter extends Router {
+  constructor() {
+    super();
+    this.on("beforeRender", (document) => {
+      document.password = undefined;
+    });
+  }
+
   applyRoutes(application: restify.Server) {
     application.get("/users", (req, res, next) => {
-      User.find().then((users) => {
-        res.json(users);
-        return next();
-      });
+      User.find().then(this.render(res, next));
     });
     application.get("/users/:id", (req, res, next) => {
-      User.findById(req.params.id).then((user) => {
-        if (user) {
-          res.json(user);
-          return next();
-        }
-        res.send(404);
-        return next();
-      });
+      User.findById(req.params.id).then(this.render(res, next));
     });
     application.post("/users", (req, res, next) => {
       let user = new User(req.body);
-      user.save().then((user) => {
-        user.password = undefined;
-        res.json(user);
-        return next();
-      });
+      user.save().then(this.render(res, next));
     });
 
     application.put("/users/:id", (req, res, next) => {
@@ -40,21 +33,13 @@ class UsersRouter extends Router {
             res.send(404);
           }
         })
-        .then((user) => {
-          res.json(user);
-          return next();
-        });
+        .then(this.render(res, next));
     });
     application.patch("/users/:id", (req, res, next) => {
       const options = { new: true };
-      User.findByIdAndUpdate(req.params.id, req.body, options).then((user) => {
-        if (user) {
-          res.json(user);
-          return next();
-        } else {
-          res.send(404);
-        }
-      });
+      User.findByIdAndUpdate(req.params.id, req.body, options).then(
+        this.render(res, next)
+      );
     });
     application.del("/users/:id", (req, res, next) => {
       User.deleteOne({ _id: req.params.id }).then((cmdResult: any) => {
